58a7e7d60a9e8acedd7ef29863c0ef0d760c2acb
stans <stans@amaterasu.dev>
2018-04-27 16:10:01 -0400

    Gets react-toolbox working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'react-toolbox/lib/button/Button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button raised primary>Hi!</Button>
+        <Button>Another Button</Button>
       </div>
     );
   }
