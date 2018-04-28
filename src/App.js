af42b2fffb78f07c46b9b079f7ba75a1901e1212
Stan Slupecki <jarlofearls@gmail.com>
2018-04-28 11:35:56 -0400

    Some basic setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import Button from 'react-toolbox/lib/button/Button';
 import logo from './logo.svg';
 import './App.css';
 
+import {LoginScreen} from './components/LoginScreen.js';
+
 class App extends Component {
+
+  constructor() {
+    super();
+    
+    this.state = {
+      username: "",
+    }
+
+    // bind functions
+    this.handleChange = this.handleChange.bind(this);
+    this.getValue = this.getValue.bind(this);
+  }
+
+  handleChange = (name, value) => {
+    this.setState({...this.setState, [name]: value});
+  }
+
+  getValue = (value) => {
+    return this.state[value];
+  }
+
   render() {
     return (
       <div className="App">
-        <Button raised primary>Hi!</Button>
-        <Button>Another Button</Button>
+        <section>
+        <LoginScreen handleChange={this.handleChange}
+        getValue={this.getValue}/>
+        </section>
       </div>
     );
   }
